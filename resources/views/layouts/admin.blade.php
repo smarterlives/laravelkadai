100c04849cb4545823f27bbf139494e736e8443f
smarterlives <masa88pride@gmail.com>
2019-11-16 04:36:05 +0000

    laravel kadai10

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang"{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        
+        <title>@yield('title')</title>
+        
+        <!-- Scripts -->
+        <script src="{{ secure_asset('js/app.js') }}" defer></script>
+        
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+        <link href="https//fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+        
+        <!-- Styles -->
+        <link href="{{ secure_asset('css/app.css')}}" rel="stylesheet">
+        <link href="{{ secure_asset('css/admin.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div id="app">
+            <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
+                <div class="container">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
+                </div>
+            </nav>
+        </div>
+    </body>
+    
+</html>
