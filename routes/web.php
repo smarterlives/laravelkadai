6a3ea729226efce30e0f8a04f03cfd28db08f4f6
smarterlives <masa88pride@gmail.com>
2019-12-23 13:12:38 +0000

    test

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('admin/news/create', 'Admin\NewsController@add')->middleware('auth');
 
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
