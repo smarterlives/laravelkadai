a19c022ccf585eedc700354361003957df8ed174
smarterlives <masa88pride@gmail.com>
2019-12-29 07:47:10 +0000

    laravel14

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Profile;
 
 class ProfileController extends Controller
 {
@@ -14,6 +15,28 @@ class ProfileController extends Controller
     }
     public function create(Request $request)
   {
+      $this->validate($request, Profile::$rules);
+
+      $news = new Profile;
+      $form = $request->all();
+
+      // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
+      if (isset($form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $news->image_path = basename($path);
+      } else {
+          $news->image_path = null;
+      }
+
+      // フォームから送信されてきた_tokenを削除する
+      unset($form['_token']);
+      // フォームから送信されてきたimageを削除する
+      unset($form['image']);
+
+      // データベースに保存する
+      $news->fill($form);
+      $news->save();
+      
       return redirect('admin/profile/create');
   }  
 }   
